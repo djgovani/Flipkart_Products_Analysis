5f70fb8792d341849d339c93f6fed31e1c47b681
deep <deep@improwised.com>
2018-01-29 18:44:10 +0530

    Added product_rating to display product ratings

--- DataPrinter.js
+++ DataPrinter.js
@@ -1,9 +1,9 @@
 let Table = require('cli-table');
 
-let toRecord = parseInt(process.argv[2], 0);
-let fromRecord = parseInt(process.argv[3], 0);
-let rc = fromRecord - toRecord; //Count record which will display
-
+let toRecord = parseInt(process.argv[3], 0);
+let fromRecord = parseInt(process.argv[4], 0);
+let totalRC = fromRecord - toRecord; //Count Total record which will display
+let rc = 0; // Record counter
 /* |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
  * | Show Product Details with Discount Percentage |
  * |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
@@ -29,10 +29,10 @@ let pr = 0; //count recod which having discount percentage
           //Checking isNan or not
         } else {
           table.push([product.brand, product.product_name, product.product_category_tree, product.price, product.discounted_price, discountPer + ' %']);
-          pr++;
+          rc++;
         }
       });
-      console.log('Displaying ' +pr + ' Records having discount % out of ' +rc + ' records'+' out of ' + products.length);
+      console.log('Displaying ' +rc + ' Records having discount % out of ' +totalRC + ' records'+' out of ' + products.length);
     }
     console.log(table.toString());
   } else {
@@ -61,7 +61,7 @@ exports.productDetails = (products) => {
       products.slice(toRecord, fromRecord).forEach((product) => {
         table.push([product.brand, product.product_name, product.product_category_tree, product.price, product.discounted_price]);
       });
-      console.log('Displaying ' +rc + ' records'+' out of ' + products.length);
+      console.log('Displaying ' +totalRC + ' records'+' out of ' + products.length);
     } else {
       products.forEach((product) => {
         table.push([product.brand, product.product_name, product.product_category_tree, product.price, product.discounted_price]);
@@ -101,14 +101,14 @@ let filteredRc = 0; // Count filtered record
       */
       if ((product.price >= parseInt(process.argv[3], 0) && product.price <= parseInt(process.argv[4], 0)) && (product.brand == process.argv[5]) && ((spProCat[1] == process.argv[6]) || (spProCat[2] == process.argv[6]) || (spProCat[3] == process.argv[6]))) {
         table.push([product.brand, product.product_name, product.product_category_tree, product.price, product.discounted_price]);
-        filteredRc++;
+        rc++;
       } else if ((product.price >= parseInt(process.argv[3], 0) && product.price <= parseInt(process.argv[4], 0)) && (product.brand == process.argv[5])){
         table.push([product.brand, product.product_name, product.product_category_tree, product.price, product.discounted_price]);
-        filteredRc++;
+        rc++;
       }
     });
 
-    console.log('Displaying ' +filteredRc + ' records'+' out of ' + products.length);
+    console.log('Displaying ' +rc + ' records'+' out of ' + products.length);
     console.log(table.toString());
   } else {
     console.log('--------------------');
@@ -116,3 +116,35 @@ let filteredRc = 0; // Count filtered record
     console.log('--------------------');
   }
 }
+
+/* |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+ * | Show Product Details with Ratings                |
+ * |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+ */
+exports.productRatingDetails = (products) => {
+  if (products.length > 0) {
+    let table = new Table({
+      head: ['Brand', 'Product Name', 'Product Category Tree', 'Price', 'Discounted_Price', 'Product_rating'],
+      colWidths: [25, 65, 70, 10, 20, 20],
+    });
+
+    if (!fromRecord) {
+      products.slice(0, toRecord).forEach((product) => {
+          table.push([product.brand, product.product_name, product.product_category_tree, product.price, product.discounted_price]);
+      });
+    } else if (fromRecord) {
+      products.slice(toRecord, fromRecord).forEach((product) => {
+        if ((product.product_rating !== 'No rating available') && (product.product_rating !== 'Flat Top Buffing Foundation Brush') && (product.product_rating !== 'hrung in die Kernphysik for Rs.1805 online. Einf&uuml') ) {
+        table.push([product.brand, product.product_name, product.product_category_tree, product.price, product.discounted_price, product.product_rating]);
+        rc++;
+      }
+      });
+    }
+    console.log('Displaying ' +rc + ' records'+' out of ' + products.length);
+    console.log(table.toString());
+  } else {
+    console.log('--------------------');
+    console.log('No product Found.');
+    console.log('--------------------');
+  }
+};
