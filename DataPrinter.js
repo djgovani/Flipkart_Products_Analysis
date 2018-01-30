4770f2d52acbc6da01c95568899e790107e5660a
deep <deep@improwised.com>
2018-01-30 13:14:37 +0530

    lint error fixed

--- DataPrinter.js
+++ DataPrinter.js
@@ -2,16 +2,15 @@ let Table = require('cli-table');
 
 let toRecord = parseInt(process.argv[3], 0);
 let fromRecord = parseInt(process.argv[4], 0);
-let totalRC = fromRecord - toRecord; //Count Total record which will display
+let totalRC = fromRecord - toRecord; // Count Total record which will display
 let rc = 0; // Record counter
+
 /* |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
  * | Show Product Details with Discount Percentage |
  * |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
  */
 exports.productDetailsPr = (products) => {
-let pr = 0; //count recod which having discount percentage
   if (products.length > 0) {
-
     let table = new Table({
       head: ['Brand', 'Product Name', 'Product Category Tree', 'Price', 'Discounted_Price', 'Discount %'],
       colWidths: [25, 65, 70, 10, 20, 20],
@@ -19,20 +18,20 @@ let pr = 0; //count recod which having discount percentage
 
     if (!fromRecord) {
       products.slice(0, toRecord).forEach((product) => {
-        let discountPer =((((product.price - product.discounted_price)/product.price)*100).toFixed(2));
+        let discountPer = ((((product.price - product.discounted_price) / product.price) * 100).toFixed(2));
         table.push([product.brand, product.product_name, product.product_category_tree, product.price, product.discounted_price, discountPer + ' %']);
       });
     } else {
       products.slice(toRecord, fromRecord).forEach((product) => {
-        let discountPer =((((product.price - product.discounted_price)/product.price)*100).toFixed(2));
+        let discountPer = ((((product.price - product.discounted_price) / product.price) * 100).toFixed(2));
         if (isNaN(discountPer)) {
-          //Checking isNan or not
-        } else {
+          // Checking isNan or not
+        } else if ((/\S/.test(product.brand))) {
           table.push([product.brand, product.product_name, product.product_category_tree, product.price, product.discounted_price, discountPer + ' %']);
-          rc++;
+          rc += 1;
         }
       });
-      console.log('Displaying ' +rc + ' Records having discount % out of ' +totalRC + ' records'+' out of ' + products.length);
+      console.log('Displaying ' + rc + ' Records having discount % out of ' + totalRC + ' records out of ' + products.length);
     }
     console.log(table.toString());
   } else {
@@ -55,13 +54,13 @@ exports.productDetails = (products) => {
 
     if (!fromRecord) {
       products.slice(0, toRecord).forEach((product) => {
-          table.push([product.brand, product.product_name, product.product_category_tree, product.price, product.discounted_price]);
+        table.push([product.brand, product.product_name, product.product_category_tree, product.price, product.discounted_price]);
       });
     } else if (fromRecord) {
       products.slice(toRecord, fromRecord).forEach((product) => {
         table.push([product.brand, product.product_name, product.product_category_tree, product.price, product.discounted_price]);
       });
-      console.log('Displaying ' +totalRC + ' records'+' out of ' + products.length);
+      console.log('Displaying ' + totalRC + ' records out of ' + products.length);
     } else {
       products.forEach((product) => {
         table.push([product.brand, product.product_name, product.product_category_tree, product.price, product.discounted_price]);
@@ -80,42 +79,40 @@ exports.productDetails = (products) => {
  * |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
  */
 exports.productFilter = (products) => {
-let filteredRc = 0; // Count filtered record
   if (products.length > 0) {
     let table = new Table({
       head: ['Brand', 'Product Name', 'Product Category Tree', 'Price', 'Discounted_Price'],
       colWidths: [25, 65, 70, 10, 20],
     });
 
-    //products.slice(toRecord, fromRecord).forEach((product) => {
-      products.forEach((product) => {
-
+    // products.slice(toRecord, fromRecord).forEach((product) => {
+    products.forEach((product) => {
       let spProCat = product.product_category_tree.split(/\s*>>\s*/); // Split the product category by >>
 
-      /*|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-       *| process.argv[3] have minPrice         |
-       *| process.argv[4] have maxPrice         |
-       *| process.argv[5] have the Brand name   |
-       *| process.argv[6] have Product Category |
-       *~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-      */
-      if ((product.discounted_price >= parseInt(process.argv[3], 0) && product.discounted_price <= parseInt(process.argv[4], 0)) && (product.brand == process.argv[5]) && ((spProCat[1] == process.argv[6]) || (spProCat[2] == process.argv[6]) || (spProCat[3] == process.argv[6]))) {
+      /* |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+       * | process.argv[3] have minPrice         |
+       * | process.argv[4] have maxPrice         |
+       * | process.argv[5] have the Brand name   |
+       * | process.argv[6] have Product Category |
+       * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+       */
+      if ((product.discounted_price >= parseInt(process.argv[3], 0) && product.discounted_price <= parseInt(process.argv[4], 0)) && (product.brand === process.argv[5]) && ((spProCat[1] === process.argv[6]) || (spProCat[2] === process.argv[6]) || (spProCat[3] === process.argv[6]))) {
         table.push([product.brand, product.product_name, product.product_category_tree, product.price, product.discounted_price]);
-        rc++;
-      } else if ((product.discounted_price >= parseInt(process.argv[3], 0) && product.discounted_price <= parseInt(process.argv[4], 0)) && (product.brand == process.argv[5])){
+        rc += 1;
+      } else if ((product.discounted_price >= parseInt(process.argv[3], 0) && product.discounted_price <= parseInt(process.argv[4], 0)) && (product.brand === process.argv[5])){
         table.push([product.brand, product.product_name, product.product_category_tree, product.price, product.discounted_price]);
-        rc++;
+        rc += 1;
       }
     });
 
-    console.log('Displaying ' +rc + ' records'+' out of ' + products.length);
+    console.log('Displaying ' + rc + ' records out of ' + products.length);
     console.log(table.toString());
   } else {
     console.log('--------------------');
     console.log('No product Found.');
     console.log('--------------------');
   }
-}
+};
 
 /* |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
  * | Show Product Details with Ratings                |
@@ -130,17 +127,20 @@ exports.productRatingDetails = (products) => {
 
     if (!fromRecord) {
       products.slice(0, toRecord).forEach((product) => {
-          table.push([product.brand, product.product_name, product.product_category_tree, product.price, product.discounted_price]);
+        if ((product.product_rating !== 'No rating available') && (product.product_rating !== 'Flat Top Buffing Foundation Brush') && (product.product_rating !== 'hrung in die Kernphysik for Rs.1805 online. Einf&uuml')) {
+          table.push([product.brand, product.product_name, product.product_category_tree, product.price, product.discounted_price, product.product_rating]);
+          rc += 1;
+        }
       });
     } else if (fromRecord) {
       products.slice(toRecord, fromRecord).forEach((product) => {
-        if ((product.product_rating !== 'No rating available') && (product.product_rating !== 'Flat Top Buffing Foundation Brush') && (product.product_rating !== 'hrung in die Kernphysik for Rs.1805 online. Einf&uuml') ) {
-        table.push([product.brand, product.product_name, product.product_category_tree, product.price, product.discounted_price, product.product_rating]);
-        rc++;
-      }
+        if ((/\S/.test(product.brand)) && (product.product_rating !== 'No rating available') && (product.product_rating !== 'Flat Top Buffing Foundation Brush') && (product.product_rating !== 'hrung in die Kernphysik for Rs.1805 online. Einf&uuml')) {
+          table.push([product.brand, product.product_name, product.product_category_tree, product.price, product.discounted_price, product.product_rating]);
+          rc += 1;
+        }
       });
     }
-    console.log('Displaying ' +rc + ' records'+' out of ' + products.length);
+    console.log('Displaying ' + rc + ' records out of ' + products.length);
     console.log(table.toString());
   } else {
     console.log('--------------------');
