43de5c93bc7c431a15cb4fc0f1f23827be22f0a8
deep <deep@improwised.com>
2018-01-31 17:32:22 +0530

    Added sort module for product rating

---
+++ sortByProductRating.js
@@ -0,0 +1,24 @@
+/* |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+ * | Sort the Product Rating in ascending and descending order |
+ * |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+ */
+exports.sortProductRating = (prodctRating) => {
+  prodctRating.forEach((product) => {
+    delete product.discount_percentage;
+  });
+
+  let byProductRating = prodctRating.slice(0);
+  byProductRating.sort((pr1, pr2) => {
+    if (process.argv[2] === '5') {
+      return pr1.product_rating < pr2.product_rating ? -1 : pr1.product_rating > pr2.product_rating ? 1 : 0;
+    } else if (process.argv[2] === '6') {
+      return pr1.product_rating > pr2.product_rating ? -1 : pr1.product_rating < pr2.product_rating ? 1 : 0;
+    }
+  });
+  let filterProductRating = byProductRating.filter((product) => {
+    if ((product.product_rating !== 'No rating available') && (product.product_rating !== 'Flat Top Buffing Foundation Brush') && (product.product_rating !== 'hrung in die Kernphysik for Rs.1805 online. Einf&uuml')) {
+      return product;
+    }
+  });
+  return filterProductRating;
+};
