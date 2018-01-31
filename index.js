9ebaaf11b4fedb4d934a1993242e177d54e94768
deep <deep@improwised.com>
2018-01-31 16:48:27 +0530

    Remove the sorting function

--- index.js
+++ index.js
@@ -1,108 +1,17 @@
-let csv = require('fast-csv');
-let assert = require('assert');
-let fs = require('fs');
-let print = require('./DataPrinter');
-let filePath = './Product_Data/data.csv';
-
-/* |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
- * |   Table in ascending order of prodcuct rating |
- * |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
- */
-const sortProRate = (proRate) => {
-  console.log('Table in ascending order of product_rating');
-  let byProRate = proRate.slice(0);
-  byProRate.sort((pr1, pr2) => {
-    return pr1.product_rating < pr2.product_rating ? -1 : pr1.product_rating > pr2.product_rating ? 1 : 0;
-  });
-
-  // print with the product_rating
-  print.productRatingDetails(byProRate);
-};
-
-/* |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
- * |  Table in descending order of prodcuct rating |
- * |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
- */
-const sortProRateR = (proRate) => {
-  console.log('Table in descending order of product_rating');
-  let byProRate = proRate.slice(0);
-  byProRate.sort((pr1, pr2) => {
-    return pr1.product_rating > pr2.product_rating ? -1 : pr1.product_rating < pr2.product_rating ? 1 : 0;
-  });
-
-  // print with the product_rating
-  print.productRatingDetails(byProRate);
-};
-
-/* |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
- * |      Table in ascending order of Brand        |
- * |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
- */
-const sortBrandName = (proBrand) => {
-  let byBrandName = proBrand.slice(0);
-  byBrandName.sort((br1, br2) => {
-    return br1.brand.toLowerCase() < br2.brand.toLowerCase() ? -1 : br1.brand.toLowerCase() > br2.brand.toLowerCase() ? 1 : 0;
-  });
-  if (process.argv[2] === '5') {
-    sortProRate(byBrandName);
-  } else if (process.argv[2] === '6') {
-    sortProRateR(byBrandName);
-  } else {
-    console.log('Table in ascending order of Brand');
-    // print without the discount percentage
-    print.productDetails(byBrandName);
-  }
-};
-
-/* |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
- * |      Table in descending order of Brand       |
- * |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
- */
-const sortBrandNameR = (proBrand) => {
-  let byBrandName = proBrand.slice(0);
-  byBrandName.sort((br1, br2) => {
-    return br1.brand.toLowerCase() > br2.brand.toLowerCase() ? -1 : br1.brand.toLowerCase() < br2.brand.toLowerCase() ? 1 : 0;
-  });
-  console.log('Table in descending order of Brand');
-  // print without the discount percentage
-  print.productDetails(byBrandName);
-};
-
-/* |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
- * |   Table in ascending order of discount(in %)  |
- * |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
- */
- const sortDisPer = (dispr) => {
-  console.log('Table in ascending order of discount(in %)');
-  let byDisPer = dispr.slice(0);
-  byDisPer.sort((dp1, dp2) => {
-    return dp1.discountPer < dp2.discountPer ? -1 : dp1.discountPer > dp2.discountPer ? 1 : 0;
-  });
-
-  // print with the discount percentage
-  print.productDetailsPr(byDisPer);
-};
-
-/* |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
- * |  Table in descending order of discount(in %)  |
- * |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
- */
- const sortDisPerR = (dispr) => {
-  console.log('Table in descending order of discount(in %)');
-  let byDisPer = dispr.slice(0);
-  byDisPer.sort((dp1, dp2) => {
-    return dp1.discountPer > dp2.discountPer ? -1 : dp1.discountPer < dp2.discountPer ? 1 : 0;
-  });
-
-  // print with the discount percentage
-  print.productDetailsPr(byDisPer);
-};
+const csv = require('fast-csv');
+const assert = require('assert');
+const fs = require('fs');
+const print = require('./DataPrinter');
+const sortByBrand = require('./sortByBrand');
+const sortByDiscountPercentage = require('./sortByDicountPercentage');
+const sortByProductRating = require('./sortByProductRating');
+const productFilter = require('./productFilter');
+const filePath = './Product_Data/data.csv';
 
 /* |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
  * |                File Parser                    |
  * |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
  */
-// exports.file = (callback) => {
 const fileParser = () => {
   assert.ok(filePath, 'filePath is required');
   let parsedProducts = [];
@@ -117,7 +26,7 @@ const fileParser = () => {
           product_category_tree: data[5],
           price: data[7],
           discounted_price: data[8],
-          discountPer: (((data[7] - data[8]) / data[7]) * 100).toFixed(2),
+          discount_percentage: (((data[7] - data[8]) / data[7]) * 100).toFixed(2),
           product_rating: data[12],
         });
       })
@@ -127,9 +36,9 @@ const fileParser = () => {
       });
   });
 
-  promise.then((message) => {
+  promise.then(() => {
     /*
-      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+      |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
       |  call the function by checking the arguments      |
       |  1 - Table in ascending order of Brand            |
       |  2 - Table in descending order of Brand           |
@@ -138,26 +47,25 @@ const fileParser = () => {
       |  5 - Table in ascending order of product rating   |
       |  6 - Table in descending order of product rating  |
       |  7 - Table will show the filtered products        |
-      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+      |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
     */
     if (process.argv[2] === '1') {
-      sortBrandName(parsedProducts);
+      print.showProducts(sortByBrand.sortBrandName(parsedProducts));
     } else if (process.argv[2] === '2') {
-      sortBrandNameR(parsedProducts);
+      print.showProducts(sortByBrand.sortBrandName(parsedProducts));
     } else if (process.argv[2] === '3') {
-      sortDisPer(parsedProducts);
+      print.showProducts(sortByDiscountPercentage.sortDiscountPercentage(parsedProducts));
     } else if (process.argv[2] === '4'){
-      sortDisPerR(parsedProducts);
+      print.showProducts(sortByDiscountPercentage.sortDiscountPercentage(parsedProducts));
     } else if (process.argv[2] === '5'){
-      sortBrandName(parsedProducts);
+      print.showProducts(sortByProductRating.sortProductRating(parsedProducts));
     } else if (process.argv[2] === '6'){
-      sortBrandName(parsedProducts);
+      print.showProducts(sortByProductRating.sortProductRating(parsedProducts));
     } else if (process.argv[2] === '7'){
-      print.productFilter(parsedProducts);
+      print.showProducts(productFilter.productFilter(parsedProducts));
     } else {
       print.productDetails(parsedProducts);
     }
-    // callback(null, parsedProducts);
   }).catch((err) => {
     console.log(err.message);
   });
