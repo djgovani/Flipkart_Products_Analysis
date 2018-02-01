1a0b8e90da66c95a73db8c732d885f6fb44f6812
deep <deep@improwised.com>
2018-02-01 16:37:48 +0530

    added the order parameter insted of process.argv

--- sortByBrand.js
+++ sortByBrand.js
@@ -2,13 +2,13 @@
  * | Sort the Brand in ascending and descending order |
  * |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
  */
-exports.sortBrandName = (proBrand) => {
+exports.sortBrandName = (proBrand, order) => {
 
   let byBrandName = proBrand.slice(0);
   byBrandName.sort((br1, br2) => {
-    if (process.argv[2] === '1') {
+    if (order === 1) {
       return br1.brand.toLowerCase() < br2.brand.toLowerCase() ? -1 : br1.brand.toLowerCase() > br2.brand.toLowerCase() ? 1 : 0;
-    } else if (process.argv[2] === '2') {
+    } else if (order === 2) {
       return br1.brand.toLowerCase() > br2.brand.toLowerCase() ? -1 : br1.brand.toLowerCase() < br2.brand.toLowerCase() ? 1 : 0;
     }
   });
