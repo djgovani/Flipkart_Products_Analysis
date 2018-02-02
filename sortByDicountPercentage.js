e74b7bc055874d2844e9a2268978aefef0d7d4d4
deep <deep@improwised.com>
2018-02-02 12:27:03 +0530

    Remove the sorting function and add the call statement of sort function

--- sortByDicountPercentage.js
+++ sortByDicountPercentage.js
@@ -1,27 +1,19 @@
+const sort = require('./sortHelper');
+
 /* |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
  * | Sort the Discount Percentage in ascending and descending order |
  * |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
  */
-exports.sortDiscountPercentage = (dispr, order) => {
-  dispr.forEach((product) => {
+exports.sortDiscountPercentage = (discountPercentage, order) => {
+  discountPercentage.slice(0).forEach((product) => {
     delete product.product_rating;
 
-    product.discount_percentage = (((product.price - product.discounted_price) / product.price) * 100).toFixed(2);
-
+    product.discount_percentage = (((product.price - product.discounted_price) / product.price) * 100).toFixed(2); // Calculate the discount in percentage (%)
     if (isNaN(product.discount_percentage)) {
       product.discount_percentage = 0;
     } else {
       product.discount_percentage = `${product.discount_percentage} %`;
     }
   });
-
-  let byDiscountPercentage = dispr.slice(0);
-  byDiscountPercentage.sort((dp1, dp2) => {
-    if (order === 3) {
-      return dp1.discount_percentage < dp2.discount_percentage ? -1 : dp1.discount_percentage > dp2.discount_percentage ? 1 : 0;
-    } else if (order === 4) {
-      return dp1.discount_percentage > dp2.discount_percentage ? -1 : dp1.discount_percentage < dp2.discount_percentage ? 1 : 0;
-    }
-  });
-  return byDiscountPercentage;
+  return sort.sort(order, discountPercentage, 'discount_percentage');;
 };
