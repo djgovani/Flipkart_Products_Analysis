5a562bf440ee43bdd4dcbee183f45af0a36d615e
deep <deep@improwised.com>
2018-02-02 15:24:10 +0530

    lint error solved

--- sortHelper.js
+++ sortHelper.js
@@ -4,21 +4,21 @@
  */
 exports.sorting = (order, product, item) => {
   product.sort((br1, br2) => {
-    switch(order){
+    switch (order){
       case 1: return br1[item].toLowerCase() < br2[item].toLowerCase() ? -1 : br1[item].toLowerCase() > br2[item].toLowerCase() ? 1 : 0;
-      break;
+        break;
 
       case 2: return br1[item].toLowerCase() > br2[item].toLowerCase() ? -1 : br1[item].toLowerCase() < br2[item].toLowerCase() ? 1 : 0;
-      break;
+        break;
 
       case 3: return br1[item] < br2[item] ? -1 : br1[item] > br2[item] ? 1 : 0;
-      break;
+        break;
 
       case 4: return br1[item] > br2[item] ? -1 : br1[item] < br2[item] ? 1 : 0;
-      break;
+        break;
 
       default:
-      break;
+        break;
     }
   });
   return product;
