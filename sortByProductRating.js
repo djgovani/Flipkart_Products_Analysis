bd504d6d125365cb8c19b2d2534af5e7616f17eb
deep <deep@improwised.com>
2018-02-02 12:48:28 +0530

    Update the name of sort function

--- sortByProductRating.js
+++ sortByProductRating.js
@@ -9,7 +9,7 @@ exports.sortProductRating = (prodctRating, order) => {
     delete product.discount_percentage;
   });
 
-  let filterProductRating = sort.sort(order, prodctRating, 'product_rating').filter((product) => {
+  let filterProductRating = sort.sorting(order, prodctRating, 'product_rating').filter((product) => {
     if ((product.product_rating !== 'No rating available') && (product.product_rating !== 'Flat Top Buffing Foundation Brush') && (product.product_rating !== 'hrung in die Kernphysik for Rs.1805 online. Einf&uuml')) {
       return product;
     }
