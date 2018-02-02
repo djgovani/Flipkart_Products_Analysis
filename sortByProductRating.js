99927d706c85d304567093b7ed27a36b0d432efb
deep <deep@improwised.com>
2018-02-02 12:27:10 +0530

    Remove the sorting function and add the call statement of sort function

--- sortByProductRating.js
+++ sortByProductRating.js
@@ -1,3 +1,5 @@
+const sort = require('./sortHelper');
+
 /* |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
  * | Sort the Product Rating in ascending and descending order |
  * |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
@@ -7,15 +9,7 @@ exports.sortProductRating = (prodctRating, order) => {
     delete product.discount_percentage;
   });
 
-  let byProductRating = prodctRating.slice(0);
-  byProductRating.sort((pr1, pr2) => {
-    if (order === 5) {
-      return pr1.product_rating < pr2.product_rating ? -1 : pr1.product_rating > pr2.product_rating ? 1 : 0;
-    } else if (order === 6) {
-      return pr1.product_rating > pr2.product_rating ? -1 : pr1.product_rating < pr2.product_rating ? 1 : 0;
-    }
-  });
-  let filterProductRating = byProductRating.filter((product) => {
+  let filterProductRating = sort.sort(order, prodctRating, 'product_rating').filter((product) => {
     if ((product.product_rating !== 'No rating available') && (product.product_rating !== 'Flat Top Buffing Foundation Brush') && (product.product_rating !== 'hrung in die Kernphysik for Rs.1805 online. Einf&uuml')) {
       return product;
     }
