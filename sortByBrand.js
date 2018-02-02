8d6a567fd615b5305bf98286b4b34e7c0f7600ce
deep <deep@improwised.com>
2018-02-02 12:26:51 +0530

    Remove the sorting function and add the call statement of sort function

--- sortByBrand.js
+++ sortByBrand.js
@@ -1,21 +1,12 @@
+const sort = require('./sortHelper');
+
 /* |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
  * | Sort the Brand in ascending and descending order |
  * |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
  */
 exports.sortBrandName = (proBrand, order) => {
-
-  let byBrandName = proBrand.slice(0);
-  byBrandName.sort((br1, br2) => {
-    if (order === 1) {
-      return br1.brand.toLowerCase() < br2.brand.toLowerCase() ? -1 : br1.brand.toLowerCase() > br2.brand.toLowerCase() ? 1 : 0;
-    } else if (order === 2) {
-      return br1.brand.toLowerCase() > br2.brand.toLowerCase() ? -1 : br1.brand.toLowerCase() < br2.brand.toLowerCase() ? 1 : 0;
-    }
-  });
-
-  byBrandName.forEach((product) => {
-    delete product.discount_percentage;
+  proBrand.forEach((product) => {
     delete product.product_rating;
   });
-  return byBrandName;
+  return sort.sort(order, proBrand, 'brand');
 };
