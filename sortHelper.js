8d213602dfcf2822bcbab9de4c0132236413fa25
deep <deep@improwised.com>
2018-02-02 12:24:25 +0530

    Added sortHelper module to sort the data

---
+++ sortHelper.js
@@ -0,0 +1,25 @@
+/* |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+ * | Sort the Product item in ascending and descending order |
+ * |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+ */
+exports.sort = (order, product, item) => {
+  product.sort((br1, br2) => {
+    switch(order){
+      case 1: return br1[item].toLowerCase() < br2[item].toLowerCase() ? -1 : br1[item].toLowerCase() > br2[item].toLowerCase() ? 1 : 0;
+      break;
+
+      case 2: return br1[item].toLowerCase() > br2[item].toLowerCase() ? -1 : br1[item].toLowerCase() < br2[item].toLowerCase() ? 1 : 0;
+      break;
+
+      case 3: return br1[item] < br2[item] ? -1 : br1[item] > br2[item] ? 1 : 0;
+      break;
+
+      case 4: return br1[item] > br2[item] ? -1 : br1[item] < br2[item] ? 1 : 0;
+      break;
+
+      default:
+      break;
+    }
+  });
+  return product;
+};
